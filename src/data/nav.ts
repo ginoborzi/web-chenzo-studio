d13e40a1114c1f11e3178a925f170bbf9e2333cc
Franco Borzi <francoborzi@hotmail.com>
2026-09-09 13:52:15 -0300

    fix(): change button Hablemos to make it ghost and prioritice the Agenda una llamada to make it pop and link a wpp message

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -3,7 +3,14 @@ export interface NavLink {
 	label: string;
 }
 
-export const navLinks: NavLink[] = [
+export const navLinksHeader: NavLink[] = [
+	{ href: '#servicios', label: 'Servicios' },
+	{ href: '#para-quien', label: 'Para quién' },
+	{ href: '#incluye', label: 'Qué incluye' },
+	//{ href: '#contacto', label: 'Contacto' },
+];
+
+export const navLinksFooter: NavLink[] = [
 	{ href: '#servicios', label: 'Servicios' },
 	{ href: '#para-quien', label: 'Para quién' },
 	{ href: '#incluye', label: 'Qué incluye' },
